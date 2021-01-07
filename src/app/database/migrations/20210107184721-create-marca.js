f07170a9c72446915683cbeba8564e8b49c2bd63
ednilsonmcs <ednilsonmcs@gmail.com>
2021-01-07 19:52:42 -0300

    Configuração da API e ORM; Criação de Models e Migration

---
+++ src/app/database/migrations/20210107184721-create-marca.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Marcas', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Marcas');
+  }
+};
